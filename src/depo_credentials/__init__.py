b3c10f0448dc50a0165b9c64aa850a9187432049
Hutton, Rob <Rob.Hutton@primerica.com>
2019-12-31 12:56:26 -0500

    Add verbose_name to app config

--- src/depo_credentials/__init__.py
+++ src/depo_credentials/__init__.py
@@ -1,1 +1,2 @@
 # module
+default_app_config = 'depo_credentials.apps.depo_credentialsConfig'
---
+++ src/depo_credentials/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+class depo_credentialsConfig(AppConfig):
+    name = 'depo_credentials'
+    verbose_name = _("Credentials")
